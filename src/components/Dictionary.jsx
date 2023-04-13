5a03cdf76e07b114de0e81a21a3b85127863da24
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2023-04-13 19:12:51 +0500

    bug fixed

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -1,89 +1,40 @@
 import React, { useState } from 'react';
 
-const sdata = [
-  {
-    word: 'hello',
-    phonetic: 'həˈləʊ',
-    phonetics: [
-      {
-        text: 'həˈləʊ',
-        audio:
-          '//ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3',
-      },
-      {
-        text: 'hɛˈləʊ',
-      },
-    ],
-    origin: 'early 19th century: variant of earlier hollo ; related to holla.',
-    meanings: [
-      {
-        partOfSpeech: 'exclamation',
-        definitions: [
-          {
-            definition: 'used as a greeting or to begin a phone conversation.',
-            example: 'hello there, Katie!',
-            synonyms: [],
-            antonyms: [],
-          },
-        ],
-      },
-      {
-        partOfSpeech: 'noun',
-        definitions: [
-          {
-            definition: 'an utterance of ‘hello’; a greeting.',
-            example: 'she was getting polite nods and hellos from people',
-            synonyms: [],
-            antonyms: [],
-          },
-        ],
-      },
-      {
-        partOfSpeech: 'verb',
-        definitions: [
-          {
-            definition: 'say or shout ‘hello’.',
-            example: 'I pressed the phone button and helloed',
-            synonyms: [],
-            antonyms: [],
-          },
-        ],
-      },
-    ],
-  },
-];
 export default function Dictionary() {
-  const [word, setWord] = useState(null);
+  const [word, setWord] = useState('');
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-  const fetchData = async () => {
-    setLoading(true);
-    setError(false);
+
+  const handleFetchData = async (url) => {
     try {
       const response = await fetch(url);
-      const data = await response.json();
-      // console.log(data)
-      setData(data);
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+      } else {
+        setError(true);
+      }
     } catch (error) {
       setError(true);
-      // alert(JSON.stringify(error))
     }
     setLoading(false);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetchData();
+    setLoading(true);
+    setError(false);
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    handleFetchData(url);
   };
 
   return (
     <div className='flex flex-col w-full min-h-[50px]'>
       <h3 className='text-2xl p-2 font-semibold'>Dictionary</h3>
-      <form className='p-2 flex w-' onSubmit={handleSubmit}>
+      <form className='p-2 flex w-full justify-around' onSubmit={handleSubmit}>
         <input
-          className='p-1 m-2  cursor-pointer border-r-2 border-blue-400 rounded-lg  border-2'
+          className='p-1 m-2 w-full cursor-pointer border-blue-400 rounded-lg  border'
           type='text'
           value={word}
           onChange={(e) => setWord(e.target.value)}
@@ -92,47 +43,43 @@ export default function Dictionary() {
         />
         <button
           className='  font-bold  p-1 m-2 w-36 cursor-pointer bg-slate-400  border rounded-md
-        py-1 px-4
-        '
+          py-1 px-4'
           type='submit'>
           Search
         </button>
       </form>
       {loading && <p>Loading...</p>}
       {error && <p>Something went wrong</p>}
-      {/* {data && <p>{JSON.stringify(data)}</p>} */}
       {data && (
         <div className='p-4 text-start dict'>
-          {data?.map(
-            ({ word, phonetic, phonetics, origin, meanings }, index) => (
-              <div key={index}>
-                <h2>Word: {word}</h2>
-                <p>phonetic: {phonetic}</p>
-                <p>origin: {origin}</p>
-                {phonetics?.map(({ text, audio }, index) => (
-                  <div key={index}>
-                    <p>Text: {text}</p>
-                    <audio src={audio} controls />
-                  </div>
-                ))}
-                {meanings?.map(({ partOfSpeech, definitions }, index) => (
-                  <div className='border-t-2 my-2' key={index}>
-                    <h3>{partOfSpeech}</h3>
-                    {definitions?.map(
-                      ({ definition, example, synonyms, antonyms }, index) => (
-                        <div className='border-t-2 my-2' key={index}>
-                          <p>definition: {definition}</p>
-                          <p>example: {example}</p>
-                          <p>synonyms:{JSON.stringify(synonyms)}</p>
-                          <p>antonyms:{JSON.stringify(antonyms)}</p>
-                        </div>
-                      )
-                    )}
-                  </div>
-                ))}
-              </div>
-            )
-          )}
+          {data.map(({ word, phonetic, phonetics, origin, meanings }) => (
+            <div key={word}>
+              <h2>Word: {word}</h2>
+              <p>phonetic: {phonetic}</p>
+              <p>origin: {origin}</p>
+              {phonetics?.map(({ text, audio }) => (
+                <div key={text}>
+                  <p>Text: {text}</p>
+                  <audio src={audio} controls />
+                </div>
+              ))}
+              {meanings?.map(({ partOfSpeech, definitions }) => (
+                <div className='border-t-2 my-2' key={partOfSpeech}>
+                  <h3>{partOfSpeech}</h3>
+                  {definitions?.map(
+                    ({ definition, example, synonyms, antonyms }, index) => (
+                      <div className='border-t-2 my-2' key={index}>
+                        <p>definition: {definition}</p>
+                        {example && <p>example: {example}</p>}
+                        {synonyms && <p>synonyms:{JSON.stringify(synonyms)}</p>}
+                        {antonyms && <p>antonyms:{JSON.stringify(antonyms)}</p>}
+                      </div>
+                    )
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
         </div>
       )}
     </div>
